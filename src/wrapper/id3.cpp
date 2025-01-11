0358ea387add056dfa8d8f77188401235514a3dc
Nicolas PARLANT <nicolas.parlant@parhuet.fr>
2025-01-11 23:01:26 +0000

    Fix c++23
    replace optional with boost::python::optional

--- src/wrapper/id3.cpp
+++ src/wrapper/id3.cpp
@@ -295,7 +295,7 @@ void exposeID3()
   {
     typedef ID3v2::AttachedPictureFrame cl;
     class_<cl, bases<ID3v2::Frame>, boost::noncopyable>
-      ("id3v2_AttachedPictureFrame", init<optional<const ByteVector &> >())
+      ("id3v2_AttachedPictureFrame", init<boost::python::optional<const ByteVector &> >())
       .DEF_SIMPLE_METHOD(textEncoding)
       .DEF_SIMPLE_METHOD(setTextEncoding)
       .DEF_SIMPLE_METHOD(mimeType)
@@ -312,7 +312,7 @@ void exposeID3()
   {
     typedef ID3v2::CommentsFrame cl;
     class_<cl, bases<ID3v2::Frame>, boost::noncopyable>
-      ("id3v2_CommentsFrame", init<optional<const ByteVector &> >())
+      ("id3v2_CommentsFrame", init<boost::python::optional<const ByteVector &> >())
       .def(init<String::Type>())
       .DEF_SIMPLE_METHOD(language)
       .DEF_SIMPLE_METHOD(setLanguage)
@@ -368,7 +368,7 @@ void exposeID3()
   {
     typedef ID3v2::TextIdentificationFrame cl;
     class_<cl, bases<ID3v2::Frame>, boost::noncopyable>
-      ("id3v2_TextIdentificationFrame", init<const ByteVector &, optional<String::Type> >())
+      ("id3v2_TextIdentificationFrame", init<const ByteVector &, boost::python::optional<String::Type> >())
       .def("setText", (void (cl::*)(const String &)) &cl::setText)
       .def("setText", (void (cl::*)(const StringList &)) &cl::setText)
       .DEF_SIMPLE_METHOD(textEncoding)
@@ -380,7 +380,7 @@ void exposeID3()
   {
     typedef ID3v2::UnsynchronizedLyricsFrame cl;
     class_<cl, bases<ID3v2::Frame>, boost::noncopyable>
-      ("id3v2_UnsynchronizedLyricsFrame", init<optional<const ByteVector &> >())
+      ("id3v2_UnsynchronizedLyricsFrame", init<boost::python::optional<const ByteVector &> >())
       .def(init<String::Type>())
       .DEF_SIMPLE_METHOD(language)
       .DEF_SIMPLE_METHOD(setLanguage)
@@ -395,7 +395,7 @@ void exposeID3()
     typedef ID3v2::UserTextIdentificationFrame cl;
     class_<cl, bases<ID3v2::TextIdentificationFrame>, boost::noncopyable>
       ("id3v2_UserTextIdentificationFrame", init<const ByteVector &>())
-      .def(init<optional<String::Type> >())
+      .def(init<boost::python::optional<String::Type> >())
       .DEF_SIMPLE_METHOD(description)
       .DEF_SIMPLE_METHOD(setDescription)
       .DEF_SIMPLE_METHOD(fieldList)
@@ -452,8 +452,8 @@ void exposeID3()
     typedef MPEG::File cl;
     class_<MPEG::File, bases<File>, boost::noncopyable>
       ("mpeg_File",
-       init<const char *, optional<bool, AudioProperties::ReadStyle> >())
-      .def(init<const char *, ID3v2::FrameFactory *, optional<bool, AudioProperties::ReadStyle> >())
+       init<const char *, boost::python::optional<bool, AudioProperties::ReadStyle> >())
+      .def(init<const char *, ID3v2::FrameFactory *, boost::python::optional<bool, AudioProperties::ReadStyle> >())
       .def("save",
 	   #if (TAGLIB_MAJOR_VERSION == 1) && (TAGLIB_MINOR_VERSION < 12)
              (bool (MPEG::File::*)(int, bool, int))

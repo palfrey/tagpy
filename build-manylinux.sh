7e8911dd099aba67f733bfef68197ebcc54c21b3
Tom Parker-Shemilt <palfrey@tevp.net>
2025-12-07 16:23:56 +0000

    Fix build-manylinux versions

--- build-manylinux.sh
+++ build-manylinux.sh
@@ -6,7 +6,7 @@ python3.10 -m pip install -r requirements-dev.txt
 
 yum install -y utf8cpp-devel
 
-for ver in {9..13}
+for ver in {10..14}
 do
     if [ ! -f wheelhouse/tagpy-*-cp3$ver* ]; then
         python3.10 ./src/builder.py --python-version 3.$ver --taglib-version=2.0.2 --build-wheel
